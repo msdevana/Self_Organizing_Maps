ec4b1234608280b0a6af3b42a6c1a40ab156962b
Manish Devana <msdevana@um-l1twjgh5.lan>
2018-11-12 21:13:36 -0500

    added som preliminary stuff

---
+++ .ipynb_checkpoints/som_generator-checkpoint.py
@@ -0,0 +1,38 @@
+"""
+Created By Manish S. Devana
+
+Generates Self Organizing Maps and gives options for training and testing with the model
+
+"""
+
+import numpy as np 
+import xarray as xr 
+import scipy 
+from som_core import *
+import dask as dk
+
+
+
+
+class SOM(object):
+    """
+    Creates a Self Organizing Map as an object
+    """
+
+    def __init__(self, grid_size, nfeatures):
+        """
+
+
+        :param grid_size: (m X n feature map size)
+        :param nfeatures: (number of features/attirbutes for each input (also the size of the weights))
+
+        """
+
+        self.n = grid_size[0]
+        self.m = grid_size[1]
+
+        self.map = np.zeros((self.m, self.n, nfeatures))
+
+
+
+
---
+++ som_core.py
@@ -0,0 +1,16 @@
+"""
+Created By: Manish Devana
+
+Core functions for a self organizing map oriented at oceanographic purposes.
+
+"""
+
+
+import numpy as np 
+import xarray as xr 
+
+
+# Functions for som class
+
+
+
